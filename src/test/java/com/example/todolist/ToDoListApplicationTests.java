5eb09ca83c558637d63cedb6edcebff08e0e20e8
Toss1ing <kirill.borisenko05@mail.ru>
2024-02-29 01:06:28 +0300

    add test into testing file

--- src/test/java/com/example/todolist/ToDoListApplicationTests.java
+++ src/test/java/com/example/todolist/ToDoListApplicationTests.java
@@ -1,9 +1,23 @@
 package com.example.todolist;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class ToDoListApplicationTests {
+
+    private final ApplicationContext context;
+
+    ToDoListApplicationTests(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Test
+    void contextLoads() {
+        assertNotNull(context);
+    }
 
 }
