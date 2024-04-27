f603fb5875d9710c2578703cd085bfccdeaa9ebc
Toss1ing <kirill.borisenko05@mail.ru>
2024-04-27 19:51:53 +0300

    Add new properties

--- src/main/java/com/example/todolist/ToDoListApplication.java
+++ src/main/java/com/example/todolist/ToDoListApplication.java
@@ -2,10 +2,12 @@ package com.example.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class ToDoListApplication {
 
+    @GetMapping(path = "/")
     public static void main(final String[] args) {
         SpringApplication.run(ToDoListApplication.class, args);
     }
